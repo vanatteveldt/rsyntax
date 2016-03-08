8beb8d00a45b15789b46e683a60549f74c5528e9
kasperwelbers <kasperwelbers@gmail.com>
2016-03-08 20:57:34 +0100

    made getWindowMatrix start-indexing-at-zero proof

--- R/query.r
+++ R/query.r
@@ -24,10 +24,14 @@ parseQueries <- function(query){
   query = gsub(' NOT ', ' &! ', query)
   
   ## also allow space as OR
-  query = gsub('([a-zA-Z0-9*?)])[ ]+([a-zA-Z0-9*?(])', '\\1 | \\2', query)
+  query = gsub('(?<=[*a-zA-Z0-9?)])[ ]+(?=[*a-zA-Z0-9?(])', ' | ', query, perl=T)
   
-  query_form = as.list(gsub('([*a-zA-Z0-9/~_-]+)', '%s', query))
-  query_terms = regmatches(query, gregexpr('([*a-zA-Z0-9/~_-]+)', query))
+  ## make " * ", as a 'find all' solution, an immediate true
+  query = tolower(query) # safety first: for the odd possibility that someone uses T or F as a query term, which would be interpreted as TRUE or FALSE
+  query = gsub('(?<= )\\*(?= )|(?<=^)\\*(?= )', 'T', query, perl=T)
+
+  query_form = as.list(gsub('([*a-z0-9/~_-]+)', '%s', query)) # note that uppercase is not replaced, to keep the TRUE
+  query_terms = regmatches(query, gregexpr('([*a-z0-9/~_-]+)', query))
   
   query_form[query_form == ''] = NA
   t(mapply(function(x,y) list(form=x, terms=y), query_form, query_terms))
@@ -44,7 +48,6 @@ evalQuery <- function(query_values, query_form){
 evalQueryMatrix <- function(qm, terms, form){
   apply(qm[,terms, drop=F], MARGIN = 1, evalQuery, query_form=form)
 }
-
 
 getTermRegex <- function(terms, default.window=50){
   terms = parseQueries(terms)
@@ -72,6 +75,10 @@ getTermRegex <- function(terms, default.window=50){
 #' @return the annotated tokens data frame
 #' @export
 codeTokens <- function(tokens, queries, default.window=25, columnname='code', filter=rep(T, nrow(tokens)), aid_var='aid', term_i_var='id', term_var='word', condition_once=FALSE, presorted=F){
+  ## consider underscores as wordboundaries (important for some parsers that chunk words together, separated by underscores)
+  if(!class(tokens[,term_var]) == 'factor') as.factor(tokens[,term_var])
+  levels(tokens[,term_var]) = gsub('_', ' ', levels(tokens[,term_var])) 
+
   ## make query forms for the indicators and conditions
   ind = parseQueries(queries$indicator)
   con = parseQueries(queries$condition)
@@ -82,8 +89,12 @@ codeTokens <- function(tokens, queries, default.window=25, columnname='code', fi
   conr = getTermRegex(queries$condition, default.window)
   query_regex = unique(rbind(indr, conr))
   
+  ## first lookup in which article at least one of the indicator terms occurs, to ignore irrelevant articles in getQueryMatrix (speeds up searches with with few indicator hits but many condition hits)
+  #article_filter = unique(tokens[tokenGrepl(indr$regex, tokens[,term_var]), aid_var])
+  article_filter = NULL
+  
   ## create matrix where rows are tokens, columns are the query terms, and cells indicate whether the query terms occur (within the given word distance) at the place of each token.
-  qm = getQueryMatrix(tokens, query_regex, aid_var, term_i_var, term_var, presorted)
+  qm = getQueryMatrix(tokens, query_regex, aid_var, term_i_var, term_var, presorted, article_filter)
   
   #message('Evaluating queries')
   if(!columnname %in% colnames(tokens)) {
@@ -92,6 +103,7 @@ codeTokens <- function(tokens, queries, default.window=25, columnname='code', fi
     tokens[,columnname] = as.character(tokens[,columnname])
     message(sprintf('Column "%s" already exists. Existing values are not removed (unless overwritten)', columnname))
   }
+  
   nqueries = nrow(queries)
   for(i in 1:nqueries){
     if(i %% 10 == 0) message(sprintf('\t%s / %s', i, nqueries))
@@ -184,32 +196,53 @@ tokenLookup <- function(tokens, article_id, term_i, aid_var='aid', term_i_var='i
 #' @param condition_once logical. If TRUE, then if an indicator satisfies its conditions once in an article, all indicators within that article are coded.
 #' @param presorted The data has to be sorted on order(aid_var, term_i_var). If this is already the case, presorted can be set to TRUE to save time (which is usefull when testing many individual queries for large tokenlists)
 #'
-#' @return
+#' @return the tokens that match the query
 #' @export
-#'
-#' @examples
 searchQuery <- function(tokens, indicator, condition='', default.window=25, meta=NULL, condition_once=FALSE, presorted=T, filter=rep(T, nrow(tokens)), aid_var='aid', term_i_var='id', term_var='word'){
   tokens$hit = NULL
   query = data.frame(code=1, indicator=indicator, condition=condition)
-  tokens = codeTokens(tokens, query, columnname = 'hit', filter=filter, aid_var='aid', term_i_var='id', term_var='word', presorted=presorted, default.window = default.window)
-  o = tokens[tokens$hit == 1, c(aid_var, term_i_var, term_var)]
+  tokens = codeTokens(tokens, query, columnname = 'hit', filter=filter, aid_var=aid_var, term_i_var=term_i_var, term_var=term_var, presorted=presorted, default.window = default.window)
+  hits = tokens[tokens$hit == 1, c(aid_var, term_i_var, term_var)]
   
-  nhits = nrow(o)
-  narts = length(unique(o[,aid_var]))
+  ## report number of hits and articles
+  nhits = nrow(hits)
+  narts = length(unique(hits[,aid_var]))
   message(sprintf('%s hit%s in %s article%s (N = %s)', nhits, ifelse(nhits==1, '', 's'), 
-                                                       narts, ifelse(narts==1, '', 's'), 
-                                                       length(unique(tokens[,aid_var]))))
-
-  if(!is.null(meta)) o = merge(meta, o, by=aid_var, all.x=F, all.y=T)
-  o
+                  narts, ifelse(narts==1, '', 's'), 
+                  length(unique(tokens[,aid_var]))))
+  if(!is.null(meta)) hits = merge(meta, hits, by=aid_var, all.x=F, all.y=T)
+  hits
 }
 
+
+#' Search for tokens in a tokenlist using indicators with conditions
+#' 
+#' Tokens need to be sorted on order(aid_var, term_i_var), or presorted needs to be set to FALSE. Note that if this function is used often, its faster to sort tokens first and use presorted = TRUE.
+#'
+#' @param tokens 
+#' @param indicator 
+#' @param condition 
+#' @param default.window 
+#' @param tokenfreq 
+#' @param keywordIC 
+#' @param kwic_nwords 
+#' @param keywordIC_sample 
+#' @param random_sample 
+#' @param meta 
+#' @param condition_once 
+#' @param presorted 
+#' @param aid_var 
+#' @param term_i_var 
+#' @param term_var 
+#'
+#' @return prints stuff to evaluate the validity of queries
+#' @export
 testQuery <- function(tokens, indicator, condition='', default.window=25, tokenfreq=T, keywordIC=T, kwic_nwords=10, keywordIC_sample=10, random_sample=T, meta=NULL, condition_once=FALSE, presorted=T, aid_var='aid', term_i_var='id', term_var='word'){
   hits = searchQuery(tokens, indicator, condition, default.window, meta=meta, aid_var=aid_var, term_i_var=term_i_var, term_var=term_var, condition_once=condition_once, presorted=presorted)
   cat('\n')
-  if(tokenfreq) reportTokenFreq(hits, aid_var, term_var)
+  if(tokenfreq & nrow(hits) > 0) reportTokenFreq(hits, aid_var, term_var)
 
-  if(keywordIC) {
+  if(keywordIC & nrow(hits) > 0) {
     if(!is.null(keywordIC_sample)) {
       if(random_sample) hits = hits[sample(1:nrow(hits), nrow(hits)),]
       hits = head(hits, keywordIC_sample) 
@@ -264,3 +297,115 @@ compareHits <- function(tokens, hits.x, hits.y, sample_n=10, aid_var='aid', term
 #   
 #  }
 #}
+
+
+
+#################################
+#################################
+## Contains the functions to create a sparse matrix giving the windows within which queries occur.
+## Please note that simplicity is sacrificed for performance without mercy
+
+getQueryMatrix <- function(tokens, query_regex, aid_var='aid', term_i_var='id', term_var='word', presorted=F, article_filter=NULL){
+  if(!presorted){
+    ## unless explicitly noted that the token list is sorted, first sort tokens by article id and word id. (keeps order to restore original order)
+    ord = order(tokens[,aid_var], tokens[,term_i_var])
+    tokens = tokens[ord,]
+  }
+  if(is.null(article_filter)){
+    tokens_filter = which(tokenGrepl(query_regex$regex, tokens[,term_var]))
+  } else {
+    ## article filter is for computational reasons only. It ignores the articles that are not selected. currently used to ignore articles in which none of the indicators occur 
+    article_filter = which(tokens[,aid_var] %in% article_filter)
+    tokens_within_article_filter = tokenGrepl(query_regex$regex, tokens[article_filter,term_var])
+    tokens_filter = article_filter[tokens_within_article_filter]
+  }
+  
+  m = getWindowMatrix(location=tokens[tokens_filter,term_i_var], 
+                      context=tokens[tokens_filter,  aid_var], 
+                      term = tokens[tokens_filter, term_var], 
+                      window.size = max(query_regex$window))
+
+  
+  
+  ## create the rows and columns for the query matrix by looking in which rows one of the terms that matches the regex is TRUE.
+  ## by using the token_filter vector, the original positions in the token list are given to query_list  
+  query_list = list()  
+  for(j in 1:nrow(query_regex)){
+    query_m = m[,tokenGrepl(query_regex$regex[j], colnames(m)),drop=F]
+    hits = Matrix::rowSums(query_m > 0 & query_m <= query_regex$window[j]) > 0 # matrix value has to be higher than 0, but not higher than the window size
+    if(sum(hits) == 0) next
+    query_list[['']] = data.frame(i = tokens_filter[hits], j = j) 
+  }
+  qm = rbind.fill(query_list)
+  qm = spMatrix(nrow(tokens), nrow(query_regex), qm$i, qm$j, rep(T, nrow(qm)))
+  colnames(qm) = query_regex$term
+  #rownames(qm) = paste(tokens[,aid_var], tokens[,term_i_var], sep='.') # currently not used, and takes up space
+  
+  if(!presorted) qm = qm[match(1:nrow(qm), ord),] # return matrix in order of input tokens
+  qm
+}
+
+tokenGrepl <- function(patterns, x, ignore.case=T, perl=F){
+  ## a wrapper for grepl that takes multiple patterns and is efficient for a character vector (x) with many duplicates
+  unique_x = unique(x)
+  pattern = paste(patterns, collapse='|')
+  relevant_terms = unique_x[grepl(pattern, unique_x, ignore.case=ignore.case, perl=perl)]
+  x %in% relevant_terms
+}
+
+getWindowMatrix <- function(location, context, term, window.size){
+  location = stretchLocation(location, context, window.size=window.size)
+  length(location)
+  
+  shifts = -window.size:window.size
+  
+  terms = unique(term)
+  term_index = match(term, terms)
+  m = locationMatrix(i=location, j=term_index, shifts, distance.as.value=T)
+  colnames(m) = terms
+  m
+}
+
+stretchLocation <- function(location, context, window.size){
+  ## makes the word location counter global, and adds dummy locations between contexts to prevent overlapping windows (so it can be used as an index). 
+  ## this way, overlapping word windows can be calculated for multiple documents within a single matrix.
+  ## location and context need to be sorted on order(context,location)!! (hence the presorted argument in getQueryMatrix)
+  
+  if(min(location) == 0) location = location + 1 ## location will be treated as an index, so it cannot be zero in r where an index starts at 1 (and some parsers start indexing at zero)
+  
+  newcontext = which(!duplicated(context)) # where does a new context start
+  context.max = location[newcontext-1] + (window.size*2)
+  multiplier_scores = cumsum(c(0,context.max)) # the amount that should be added to the location at the start of each context 
+  repeat_multiplier = c(newcontext[2:length(newcontext)], length(location)+1) - newcontext # the number of times the multiplier scores need to be repeated to match the location vector
+  multiplier_vector = rep(multiplier_scores, repeat_multiplier)
+  return(location + multiplier_vector)
+}
+
+locationMatrix <- function(i, j, shifts=0, count.once=T, distance.as.value=F){
+  mat = spMatrix(max(i), max(j))
+  
+  shifts = shifts[order(abs(shifts))] # order from 0 to higher (required if distance.as.value = T)
+  for(shift in shifts){
+    i_shift = i + shift
+    select = i_shift > 0 & i_shift <= max(i)
+    if(distance.as.value){
+      mat = mat + spMatrix(nrow=max(i), ncol=max(j), i=i_shift[select], j=j[select], rep(abs(shift)+1, sum(select))) 
+    } else{
+      mat = mat + spMatrix(nrow=max(i), ncol=max(j), i=i_shift[select], j=j[select], rep(1, sum(select)))   
+    }
+  }
+  
+  if(distance.as.value){
+    ## remove duplicates. since the stacked triples are ordered by shifts, this leaves the shortest distance to a term in case of duplicate cells
+    count.once = F
+    select = !duplicated(data.frame(mat@i, mat@j))
+    mat = spMatrix(nrow(mat), ncol(mat), mat@i[select]+1, mat@j[select]+1, mat@x[select])
+  }
+  mat
+  length(i)
+  nrow(mat)
+  mat = mat[i,]
+  mat = as(mat, 'dgCMatrix')
+  if(count.once) mat@x[mat@x>0] = 1
+  mat
+}
--- query_test.r
+++ query_test.r
@@ -1,7 +1,47 @@
 library(amcatr)
 library(rsyntax)
 
-source('R/query.r')
+tokens = read.csv('~/Dropbox/tokens/tokens/set18585_1to5000.csv')
+
+
+test = searchQuery(tokens, indicator = 'wilders', condition = 'geert OR pvv')
+test
+
+testQuery(tokens, 'wilders', 'pvv OR geert', tokenfreq = F, keywordIC = F)
+testQuery(tokens, 'wilders', 'pvv OR geert', tokenfreq = T, keywordIC = F)
+testQuery(tokens, 'wilders', 'pvv OR geert', tokenfreq = F, keywordIC = T)
+
+testQuery(tokens, 'wilders', 'pvv OR geert')
+
+testQuery(tokens, 'sap')
+testQuery(tokens, 'sap', 'jolande groenlinks')
+
+
+testQuery(tokens, 'vluchtel* bootvlucht*', '* NOT vluchtelement*')
+
+
+hits = searchQuery(tokens, 'vluchtel* bootvlucht*', '* NOT vluchtelement*')
+hits = searchQuery(tokens, 'vluchtel* bootvlucht*')
+
+
+testQuery(tokens, 'wilders', 'pvv OR geert', kwic_nwords = 50)
+
+
+testQuery(tokens, 'energie*', 'duurza* OR groen* OR milieuvriendelijk*')
+
+###
+
+
+
+
+
+
+
+
+
+
+
+
 
 conn = amcat.connect('http://preview.amcat.nl')
 tokens = amcat.gettokens(conn, project = 978, articleset = 24249, module = 'alpino', only_cached = T, page_size = 1000)
@@ -15,18 +55,6 @@ head(tokens)
 
 tokens[tokens$aid == 96469707,]
 
-test = searchQuery(tokens, 'wilders', 'geert OR pvv')
-test
-
-testQuery(tokens, 'wilders', 'pvv OR geert', tokenfreq = F, keywordIC = F)
-testQuery(tokens, 'wilders', 'pvv OR geert', tokenfreq = T, keywordIC = F)
-testQuery(tokens, 'wilders', 'pvv OR geert', tokenfreq = F, keywordIC = T)
-testQuery(tokens, 'wilders', 'pvv OR geert')
-
-testQuery(tokens, 'wilders', 'pvv OR geert', kwic_nwords = 50)
-
-
-testQuery(tokens, 'energie*', 'duurza* OR groen* OR milieuvriendelijk*')
 
 
 reportQueryResults(hits)
